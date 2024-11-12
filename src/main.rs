023451680c2f3bed069c58b480e16f469d298d73
Anna Sudol <sudolann@gmail.com>
2024-11-12 10:40:58 +0100

    added game states

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+#![warn(clippy::pedantic)]
+
 use bracket_lib::prelude::*;
+
+// START: enum
 enum GameMode {
     Menu,
     Playing,
-    Ended,
+    End,
 }
+
+// START: state
 struct State {
     mode: GameMode,
 }
@@ -14,33 +20,72 @@ impl State {
             mode: GameMode::Menu,
         }
     }
-
-    fn play(&mut self, ctx: &mut BTerm) {
+    // START: restart
+    fn restart(&mut self) {
         self.mode = GameMode::Playing;
     }
-    fn resize(&mut self, ctx: &mut BTerm) {
-        self.mode = GameMode::Ended;
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+        // END: mainprint
+        // START: mainiflet
+
+        if let Some(key) = ctx.key {// <callout id="co.flappy_state.if_let_key" />
+            match key {
+                VirtualKeyCode::P => self.restart(),// <callout id="co.flappy_state.restart" />
+                VirtualKeyCode::Q => ctx.quitting = true,// <callout id="co.flappy_state.quitting" />
+                _ => {}
+            }
+        }
+    } // Close the function
+    // END: mainiflet
+    // END: mainmenu
+
+    // START: dead
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
     }
+    // END: dead
+
+    // START: play
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
+    }
+    // END: play
 }
 
+// START: tick
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-       match self.mode {
-           GameMode::Menu => self.main_menu(ctx),
-           GameMode::Ended => self.dead(ctx),
-           GameMode::Playing => self.play(ctx)
-       }
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
     }
 }
+// END: tick
 
-fn main_menu(ctx: &mut BTerm) {
-    ctx.cls();
-    ctx.print_centered(5, "Welcome to Flappy Dragon!");
-    ctx.print_centered(8, "(P) Play Game!");
-    ctx.print_centered(9, "(Q) Quit Game!");
-
-}
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50().with_title("Flappy dragon").build()?;
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+
+    // START: mainloop
     main_loop(context, State::new())
+    // END: mainloop
 }
