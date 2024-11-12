7cbea597b3035d47f06a113a00b6bc45bcf74888
Anna Sudol <sudolann@gmail.com>
2024-11-12 10:14:30 +0100

    added main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-fn main() {
-    let my_list = [ "One", "Two", "Three" ];
-    for num in &my_list {
-        println!("{}", num);
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1,1,"Hello, world!");
     }
+}
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50().with_title("Flappy dragon").build()?;
+    main_loop(context, State{})
 }
