548182c665be8ec32552be77bf4682a22266bec8
Anna Sudol <sudolann@gmail.com>
2024-11-12 12:02:40 +0100

    update impl Obstacle

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,19 @@
 #![warn(clippy::pedantic)]
 
 use bracket_lib::prelude::*;
-use rand::random;
 
 const SCREEN_WIDTH : i32 = 80;
 const SCREEN_HEIGHT : i32 = 50;
 const FRAME_DURATION : f32 = 75.0;
-struct Obstacle {
-    x: i32,
-    gap_y: i32,
-    size: i32,
-}
 
-impl Obstacle {
-    fn new() -> Self {
-        let mut random = RandomNumberGenerator::new()
-        Obstacle {
-            x,
-            gap_y: random.range(10, 40),
-            size: i32::max(2, 20 - score)
-        }
-    }
-
-    fn render(&self, ctx: &mut BTerm, player_x: i32) {
-        let screen_x = self.x - player_x;
-        let half_size = self.size / 2;
-        for y in 0..self.gap_y - half_size {
-            ctx.set(
-                screen_x,
-                y,
-                RGB::named(RED),
-                RGB::named(BLACK),
-                to_cp437('|'),
-            );
-        }
-        for y in self.gap_y + half_size..SCREEN_HEIGHT {
-            ctx.set(
-                screen_x,
-                y,
-                RGB::named(RED),
-                RGB::named(BLACK),
-                to_cp437('|'),
-            );
-        }
-    }
-}
 struct Player {
     x: i32,
     y: i32,
-    velocity: f32
+    velocity: f32,
 }
 
 impl Player {
-    fn new(x: i32, y: i32) -> Player {
+    fn new(x: i32, y: i32) -> Self {
         Player {
             x,
             y,
@@ -60,25 +21,97 @@ impl Player {
         }
     }
 
-    fn render(&self, ctx: &mut BTerm) {
-        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
-    }
-    //The function, to_cp437(), converts a Unicode symbol from our source code to the matching Codepage-437 character number.
     fn gravity_and_move(&mut self) {
+        // Increment gravity
         if self.velocity < 2.0 {
             self.velocity += 0.2;
         }
+
+        // Apply gravity
         self.y += self.velocity as i32;
-        self.x + 1;
         if self.y < 0 {
             self.y = 0;
         }
+
+        // Move the player
+        self.x += 1;
     }
 
     fn flap(&mut self) {
         self.velocity = -2.0;
     }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('@')
+        );
+    }
 }
+
+// START: obstacle
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32
+}
+// END: obstacle
+
+// START: constructor
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score)
+        }
+    }
+    // END: constructor
+
+    // START: render
+    fn render(&mut self, ctx: &mut BTerm, player_x : i32) {
+        let screen_x = self.x - player_x;
+        let half_size = self.size / 2;
+
+        // Draw the top half of the obstacle
+        for y in 0..self.gap_y - half_size {
+            ctx.set(
+                screen_x,
+                y,
+                RED,
+                BLACK,
+                to_cp437('|'),
+            );
+        }
+
+        // Draw the bottom half of the obstacle
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            ctx.set(
+                screen_x,
+                y,
+                RED,
+                BLACK,
+                to_cp437('|'),
+            );
+        }
+    }
+    // END: render
+
+    // START: hit
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match = player.x == self.x;// <callout id="co.flappy.x_match" />
+        let player_above_gap = player.y < self.gap_y - half_size;// <callout id="co.flappy.above_gap" />
+        let player_below_gap = player.y > self.gap_y + half_size;
+        does_x_match && (player_above_gap || player_below_gap)// <callout id="co.flappy.combined" />
+    }
+    // END: hit
+}
+
 enum GameMode {
     Menu,
     Playing,
@@ -87,49 +120,56 @@ enum GameMode {
 
 // START: state
 struct State {
-    mode: GameMode,
     player: Player,
-    frame_time: f32
+    frame_time: f32,
+    obstacle: Obstacle,
+    mode: GameMode,
+    score: i32,
 }
 
 impl State {
     fn new() -> Self {
         State {
-            mode: GameMode::Menu,
+            player: Player::new(5, 25),
             frame_time: 0.0,
-            player: Player::new(1, 0),
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
+            mode: GameMode::Menu,
+            score: 0,
         }
     }
+    // END: state
+
     // START: restart
     fn restart(&mut self) {
-        self.mode = GameMode::Playing;
-        self.player = Player::new(1, 0);
+        self.player = Player::new(5, 25);
         self.frame_time = 0.0;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
+        self.mode = GameMode::Playing;
+        self.score = 0;
     }
+    // END: restart
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "Welcome to Flappy Dragon");
         ctx.print_centered(8, "(P) Play Game");
         ctx.print_centered(9, "(Q) Quit Game");
-        // END: mainprint
-        // START: mainiflet
 
-        if let Some(key) = ctx.key {// <callout id="co.flappy_state.if_let_key" />
+        if let Some(key) = ctx.key {
             match key {
-                VirtualKeyCode::P => self.restart(),// <callout id="co.flappy_state.restart" />
-                VirtualKeyCode::Q => ctx.quitting = true,// <callout id="co.flappy_state.quitting" />
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
                 _ => {}
             }
         }
-    } // Close the function
-    // END: mainiflet
-    // END: mainmenu
+    }
 
     // START: dead
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(6, &format!("You earned {} points", self.score));
+        // END: dead
         ctx.print_centered(8, "(P) Play Again");
         ctx.print_centered(9, "(Q) Quit Game");
 
@@ -141,29 +181,39 @@ impl State {
             }
         }
     }
-    // END: dead
 
-    // START: play
     fn play(&mut self, ctx: &mut BTerm) {
         ctx.cls_bg(NAVY);
         self.frame_time += ctx.frame_time_ms;
         if self.frame_time > FRAME_DURATION {
             self.frame_time = 0.0;
+
             self.player.gravity_and_move();
         }
         if let Some(VirtualKeyCode::Space) = ctx.key {
             self.player.flap();
         }
         self.player.render(ctx);
-        ctx.print(0,0, "Press SPACE to flap!");
-        if self.player.y > SCREEN_HEIGHT {
+        // START: play
+        ctx.print(0, 0, "Press SPACE to flap.");
+        ctx.print(0, 1, &format!("Score: {}", self.score)); // <callout id="co.flappy.printscore" />
+
+        self.obstacle.render(ctx, self.player.x); // <callout id="co.flappy.obstaclerender" />
+        if self.player.x > self.obstacle.x { // <callout id="co.flappy.scoreup" />
+            self.score += 1;
+            self.obstacle = Obstacle::new(
+                self.player.x + SCREEN_WIDTH, self.score
+            );
+        }
+        if self.player.y > SCREEN_HEIGHT ||
+            self.obstacle.hit_obstacle(&self.player)
+        {
             self.mode = GameMode::End;
         }
+        // END: play
     }
-    // END: play
 }
 
-// START: tick
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         match self.mode {
@@ -173,14 +223,11 @@ impl GameState for State {
         }
     }
 }
-// END: tick
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
         .build()?;
 
-    // START: mainloop
     main_loop(context, State::new())
-    // END: mainloop
 }
